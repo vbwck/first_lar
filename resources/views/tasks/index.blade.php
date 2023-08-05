966b67c726b20f56ea68913146fb9add5f655e73
vbwck <bbneogame1@gmail.com>
2023-08-05 10:15:58 +0300

    view error list and old values

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     <h2>all tasks</h2>
+    @include('common.errors')
     <form action="/tasks" method="post">
         {{ csrf_field() }}
         <label>
             Task name:
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{ old('name') }}">
         </label>
         <input type="submit">
     </form>
     <!--TODO table with tasks-->
 @endsection
-    
